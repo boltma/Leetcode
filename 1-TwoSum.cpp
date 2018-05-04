4944991cb42adc152ca210f95e9fe9c5aba265aa
Xiaoyang Ma <mxy2000ast@126.com>
2018-05-04 12:44:08 +0800

    1 - Two Sum

---
+++ 1-TwoSum.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+
+class Solution
+{
+public:
+	vector<int> twoSum (vector<int>& nums, int target)
+	{
+		map<int, int> m;
+		for (int i = 0; i < nums.size(); ++i)
+		{
+			if (!m.count (target - nums[i]))
+				m[nums[i]] = i;
+			else return vector<int> {m[target - nums[i]], i};
+		}
+		return vector<int>();
+	}
+};
